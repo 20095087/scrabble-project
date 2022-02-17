088ee866cdc9fe19c1517de5fd0808b1f33a77f6
ddzik19 <99027550+20095087@users.noreply.github.com>
2022-02-17 17:06:20 +0000

    Complete
    - Added the last functionality where if the user types in -1 then the app will close

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,15 +3,26 @@ import java.util.*
 private val input = Scanner(System.`in`);
 private val scrabbleScore = ScrabbleScore();
 
-//    - the user should be continually asked to enter a word until they type a sentinel value (e.g. -1) to end the program
-
 fun main(args: Array<String>) {
+    var word: String =""
     while (true) {
         println(scrabbleScore.letterValues())
-        print("")
+        println("")
+        println("type in -1 to exit the app")
         print("Enter a word and compute the Scrabble Score: ")
-        val word = input.nextLine()
+        word = input.nextLine()
+        // I have made many attempts at this section before looking it up on the internet
+        // trying various if statements but I completely forgot that you can
+        // break a loop to stop any process
+        // this link https://stackoverflow.com/questions/6850380/are-whiletrue-loops-so-bad
+        // made me remember about break
+        if(word == "-1"){
+            println("Exiting App")
+            break
+        }
         println("$word has the value of ${scrabbleScore.scoreWord(word)}")
+        println("")
+
     }
 }
 
